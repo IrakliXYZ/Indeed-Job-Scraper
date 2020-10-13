855c1f6b042e113c1ec97934485d2ed1de7799e8
Irakli <39024518+IrakliMa@users.noreply.github.com>
2020-10-13 17:06:42 +0000

    Getting the titles!!

--- indeed.py
+++ indeed.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://www.indeed.com/jobs?as_and=Python+Developer+Intern&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=date&psf=advsrch&from=advancedsearch"
+LIMIT = 50
+URL = f"https://www.indeed.com/jobs?as_and=Python+Developer+Intern&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit={LIMIT}&sort=date&psf=advsrch&from=advancedsearch"
 
 def extract_indeed_pages():
-  results = requests.get(INDEED_URL)
+  results = requests.get(URL)
 
   soup = BeautifulSoup(results.text, "html.parser")
 
@@ -20,3 +21,20 @@ def extract_indeed_pages():
 
   max_pages = pages[-1]
   return max_pages
+
+
+def extract_indeed_jobs(last_page):
+  jobs = []
+  for page in range(last_page):
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+
+    for result in results:
+      title = result.find("a", {"class": "jobtitle"})["title"]
+
+      print(title)
+
+  return jobs
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from indeed import extract_indeed_pages
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-max_indeed_pages = extract_indeed_pages()
 
-print(max_indeed_pages)
+last_indeed_page = extract_indeed_pages()
+
+extract_indeed_jobs(last_indeed_page)
