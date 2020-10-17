f04171cf1c7421ed47350dc3dd8371fe45fa3b1b
Irakli <39024518+IrakliMa@users.noreply.github.com>
2020-10-17 08:44:50 +0000

    Final v1.0.0 Works and outputs CSV file

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from indeed import get_jobs as get_indeed_jobs
+import csv
 
-indeed_jobs = get_indeed_jobs()
+jobs = get_indeed_jobs()
 
-print(indeed_jobs)
+
+with open("jobs.csv", "w") as csvfile:
+
+  writer = csv.writer(csvfile)
+  writer.writerow(["Title", "Company", "Location", "Link"])
+
+  for job in jobs:
+    writer.writerow(list(job.values()))
+
