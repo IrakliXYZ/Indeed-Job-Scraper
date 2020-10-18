15b4045ea6dfff411fa1d8cfa066744030cbffd3
Daniel <71951166+Myphz@users.noreply.github.com>
2020-10-18 11:38:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ jobs = get_indeed_jobs()
 
 
 with open("jobs.csv", "w") as csvfile:
-
   writer = csv.writer(csvfile)
   writer.writerow(["Title", "Company", "Location", "Link"])
   writer.writerow([list(job.values()) for job in jobs])
